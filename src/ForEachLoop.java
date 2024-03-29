59d0105bb518ca519c5cafecf4be9e00ea31251c
AndreiRadulescu05 <andrei.radulescu2@endava.com>
2024-03-29 15:24:05 +0200

    completed forLoops exercises

--- src/ForEachLoop.java
+++ src/ForEachLoop.java
@@ -1,2 +1,8 @@
 public class ForEachLoop {
+    //Ex 1
+    //Find the Maximum: Write a Java program that finds the maximum value in an array of integers.
+    // Use a for-each loop to iterate through the array.
+    // You can initialize the array with some values or take the input from the user.
+
+
 }
--- src/ForLoops.java
+++ src/ForLoops.java
@@ -6,22 +6,57 @@ public class ForLoops {
 
         Scanner scanner1 = new Scanner(System.in);
 
-        int num;
+        int number;
         do {
             System.out.print("Enter a number between 1 and 10 to show the multiplication table: ");
-            num = scanner1.nextInt();
-        } while (num < 1 || num > 10);
+            number = scanner1.nextInt();
+        } while (number < 1 || number > 10);
 
         for (int i = 1; i <= 10; ++i) {
-            System.out.printf("%d * %d = %d \n", num, i, num * i);
+            System.out.printf("%d * %d = %d \n", number, i, number * i);
         }
-        // Ex 2
-//Factorial Calculator: Create a program that calculates the factorial of a number provided by the user.
-// Use a for loop to calculate the factorial.
-// Remember, the factorial of n (n!) is the product of all positive integers less than or equal to n.
-// n! = 1*2*3*...*n
-        
-        System.out.println("Exercise 2 Loops - Factorial Calculator");
 
+// Ex 2
+
+        System.out.println("Exercise 2 Loops - Factorial Calculator");
+        Scanner scanner = new Scanner(System.in);
+
+        int factorialNumber;
+        System.out.print("Enter a positive number to calculate the factorial: ");
+
+        if (scanner.hasNextInt()) {
+            factorialNumber = scanner.nextInt();
+
+            if (factorialNumber < 0) {
+                System.out.println("Please enter a positive number.");
+            } else {
+                long result = 1;
+                for (int i = 1; i <= factorialNumber; i++) {
+                    result *= i;
+                }
+                System.out.println("The factorial of " + factorialNumber + " is " + result + ".");
+            }
+        } else {
+            System.out.println("Not a number. Please enter a valid integer.");
+        }
+//Ex 3
+
+        System.out.println("Exercitiul 3 - Countdown Timer");
+        
+        Scanner scanner2 = new Scanner(System.in);
+
+        int countTimer= scanner2.nextInt();
+
+        while (countTimer >0){
+            System.out.println("Remaining: "+countTimer+" seconds");
+            try {
+                countTimer--;
+                Thread.sleep(1000L);    // 1000L = 1000ms = 1 second
+            }
+            catch (InterruptedException e) {
+                
+            }
+        }
     }
+
 }
