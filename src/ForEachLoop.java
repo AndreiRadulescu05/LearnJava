23198c71879a59794d9f1150de9970020f414daf
AndreiRadulescu05 <andrei.radulescu2@endava.com>
2024-04-01 09:40:54 +0300

    added additional info

--- src/ForEachLoop.java
+++ src/ForEachLoop.java
@@ -1,8 +1,25 @@
 public class ForEachLoop {
-    //Ex 1
-    //Find the Maximum: Write a Java program that finds the maximum value in an array of integers.
-    // Use a for-each loop to iterate through the array.
-    // You can initialize the array with some values or take the input from the user.
+    public static void main(String[] args) {
+        //Ex 1
+        //Find the Maximum: Write a Java program that finds the maximum value in an array of integers.
+        // Use a for-each loop to iterate through the array.
+        // You can initialize the array with some values or take the input from the user.
+        System.out.println("Exercitiul 1 - Find the Maximum");
 
+
+        // Ex 2
+        // Count Vowels: Create a program that counts the number of vowels in a given string.
+        // Use a for-each loop to iterate through the characters of the string.
+        // Consider A, E, I, O, U as vowels for this exercise.
+
+        System.out.println("Exercitiul 2 - Count Vowels");
+
+        //Ex 3
+        // Sum of Even Numbers: Given an array of integers, write a Java program that calculates
+        // the sum of all even numbers in the array using a for-each loop.
+        // You can hardcode the array or take input from the user.
+        System.out.println("Exercitiul 3 - Sum of Even Numbers");
+
+    }
 
 }
--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,2 +1,22 @@
 public class WhileLoops {
+    public static void main (String [] args) {
+        // 1.	Input Summation: Write a Java program that continuously takes numbers as input from the user
+        // and adds them to a sum. The loop should continue until the user enters 0,
+        // at which point the program should print the final sum and exit.
+
+        System.out.println("Exercitiul 1 - Input Summation");
+
+        //2.	Guessing Game: Implement a guessing game where the program selects a random number between 1 and 100,
+        // and the user has to guess it. Use a while loop to allow the user unlimited attempts until t
+        // hey guess the number correctly. After each guess, the program should tell the user
+        // if the guess is too high or too low.
+        System.out.println("Exercitiul 2 - Guessing Game");
+
+        //3.	Palindrome Checker: Create a Java program that checks if a given string is a palindrome.
+        // Use a while loop to compare characters from the beginning and the end of the string, moving towards the center.
+        // A palindrome reads the same backward as forward, like "radar" or "level".
+
+        System.out.println("Exercitiul 3 - Palindrom Checker");
+
+    }
 }
