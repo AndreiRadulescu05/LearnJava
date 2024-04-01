4d135f427b6fe80a0c074fa02f687030202670b0
AndreiRadulescu05 <andrei.radulescu2@endava.com>
2024-04-01 17:08:49 +0300

    added for each loop exercises

--- src/ForEachLoop.java
+++ src/ForEachLoop.java
@@ -1,24 +1,61 @@
+import java.util.Scanner;
+
 public class ForEachLoop {
     public static void main(String[] args) {
         //Ex 1
-        //Find the Maximum: Write a Java program that finds the maximum value in an array of integers.
-        // Use a for-each loop to iterate through the array.
-        // You can initialize the array with some values or take the input from the user.
         System.out.println("Exercitiul 1 - Find the Maximum");
 
+        int[] maximumNumbers = {10, 25, 4, 60, 120, 5, 454515};
+        int max = maximumNumbers[0];
 
-        // Ex 2
-        // Count Vowels: Create a program that counts the number of vowels in a given string.
-        // Use a for-each loop to iterate through the characters of the string.
-        // Consider A, E, I, O, U as vowels for this exercise.
+        for (int num : maximumNumbers) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        System.out.println("The Maximum number from the array is: " + max);
 
-        System.out.println("Exercitiul 2 - Count Vowels");
 
-        //Ex 3
-        // Sum of Even Numbers: Given an array of integers, write a Java program that calculates
-        // the sum of all even numbers in the array using a for-each loop.
-        // You can hardcode the array or take input from the user.
-        System.out.println("Exercitiul 3 - Sum of Even Numbers");
+            // Ex 2
+            // Count Vowels: Create a program that counts the number of vowels in a given string.
+            // Use a for-each loop to iterate through the characters of the string.
+            // Consider A, E, I, O, U as vowels for this exercise.
+
+            System.out.println("Exercitiul 2 - Count Vowels");
+
+        Scanner sentence = new Scanner(System.in);
+        String str = sentence.nextLine();
+        str = str.toLowerCase();
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a'
+                    || str.charAt(i) == 'e'
+                    || str.charAt(i) == 'i'
+                    || str.charAt(i) == 'o'
+                    || str.charAt(i)  == 'u' ) {
+                count++;
+
+            }
+        }
+        System.out.println("Total number of vowels is: "+ count);
+
+
+            //Ex 3
+            // Sum of Even Numbers: Given an array of integers, write a Java program that calculates
+            // the sum of all even numbers in the array using a for-each loop.
+            // You can hardcode the array or take input from the user.
+            // System.out.println("Exercitiul 3 - Sum of Even Numbers");
+
+    int[] evenNumber = {1, 2, 6, 7, 8, 11};
+
+    int sum = 0;
+    for (int num : evenNumber) {
+        if (num % 2 == 0) {
+            sum += num;
+        }
+    }
+        System.out.println("The sum on even numbers is: " + sum);
 
     }
 
